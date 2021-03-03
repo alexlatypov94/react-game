1b51cc66dee6ea1d267572f0885f05f51fcbd4f3
Aliaksei <alex.latypov94@gmail.com>
2021-03-03 19:48:49 +0300

    fix: fixed server settings

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const config = require("config");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const app = express();
 
@@ -8,7 +9,13 @@ app.use(express.json({ extended: true }));
 
 app.use("/api/auth", require("./routes/auth.routes"));
 
-// const PORT = config.get("port") || 5000;
+if (process.env.NODE_ENV === "production") {
+    app.use("/", express.static(path.join(__dirname, "client", "dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+    });
+}
 
 async function start() {
     try {
