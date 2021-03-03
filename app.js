fb017f883c68493811c32fb184a0bd0eadf35415
Aliaksei <alex.latypov94@gmail.com>
2021-03-03 22:11:11 +0300

    fix: fixed port

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json({ extended: true }));
 
 app.use("/api/auth", require("./routes/auth.routes"));
-
+const PORT = process.env.PORT || 5000;
 if (process.env.NODE_ENV === "production") {
     app.use("/", express.static(path.join(__dirname, "client", "dist")));
 
@@ -24,8 +24,8 @@ async function start() {
             useUnifiedTopology: true,
             useCreateIndex: true,
         });
-        app.listen(5000, () =>
-            console.log(`App has been started on port ${5000}`)
+        app.listen(PORT, () =>
+            console.log(`App has been started on port ${PORT}`)
         );
     } catch (e) {
         console.log("Server error", e.message);
